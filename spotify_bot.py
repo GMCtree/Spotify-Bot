11d46bb43e2c7ba5633e0e110099bca35ad3230b
Gabriel Ciavaglia <gciavaglia95@gmail.com>
2017-05-13 12:26:47 -0400

    completely refactored bot to be a Telegram Inline bot

--- spotify_bot.py
+++ spotify_bot.py
@@ -1,4 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from uuid import uuid4
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+from telegram import InputTextMessageContent, InlineQueryResultArticle
 import urllib.request
 import logging
 import json
@@ -9,103 +12,92 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-# contains all types of content as outlined in Spotify Web API
-types = ['album', 'artist', 'playlist', 'track']
-
 def help(bot, update):
-	print ("Help page selected")
-	bot.sendMessage(update.message.chat_id, text = "To make a general search, just enter a search query. To search specifically, type 'artist, album, playlist, or track': query. Example --> artist: Dr. Dre")
+    print ("Help page selected")
+    bot.sendMessage(update.message.chat_id, text = "To search for a song on Spotify, just call the bot by typing @spotify_telegram_bot and then typing your query. Then, just select what category you would like to search under!")
 
 def about(bot, update):
-	print ("About page selected")
-	bot.sendMessage(update.message.chat_id, text = "This bot has been created by GMCtree using Python and the Python Telegram Bot API the Python-Telegram-Bot Team")
-
-def check_search_type(query):
-	if query.split(': ')[0] in types:
-		return True
-	else:
-		return False
-
-def format_and_send(bot, update, content_data, is_specific_search, search_type = None):
-	bot_response = "Click this link to open the "
-
-	# if user has made a specific search
-	if is_specific_search:
-		# add 's' to be able to access value in Spotify response JSON
-		bot.sendMessage(update.message.chat_id, text = bot_response + search_type + " " + content_data[search_type + 's']['items'][0]['external_urls']['spotify'])
-	# if user has made a general search
-	else:
-		for cur_type in types:
-			bot.sendMessage(update.message.chat_id, text = bot_response + cur_type + " " + content_data[cur_type + 's']['items'][0]['external_urls']['spotify'])
+    print ("About page selected")
+    bot.sendMessage(update.message.chat_id, text = "This bot has been created by GMCtree using Python and the Python Telegram Bot API the Python-Telegram-Bot Team")
 
 def no_results(response):
     return all(not response[_type + "s"]['items'] for _type in types)
 
-def search(bot, update):
-	message_list = (update.message.text).split(': ')
+def search(query, query_type):
+    print("Search attempted")
 
-	is_specific_search = check_search_type(update.message.text)
+    search_query = query.lower().strip().replace(' ', '%20')
+    api_base_url = "https://api.spotify.com/v1/search?q="
+    search_types = {
+    'track' : api_base_url + search_query + "&type=track&limit=1",
+    'artist' : api_base_url + search_query + "&type=artist&limit=1",
+    'album' : api_base_url + search_query + "&type=album&limit=1",
+    'playlist' : api_base_url + search_query + "&type=playlist&limit=1"
+    }
 
-	if is_specific_search:
-		print ("Specific search selected")
-		search_type = message_list[0]
-		# replace all spaces with '%20' as per the Spotify Web API protocol
-		search_query = message_list[1].lower().strip().replace(' ', '%20')
-		request = urllib.request.Request("https://api.spotify.com/v1/search?q=" + search_query + "&type=" + search_type + "&limit=1")
-	else:
-		print ("General search selected")
-		# replace all spaces with '%20' as per the Spotify Web API protocol
-		search_query = message_list[0].lower().strip().replace(' ', '%20')
-		request = urllib.request.Request("https://api.spotify.com/v1/search?q=" + search_query + "&type=artist,album,playlist,track&limit=1")
+    search_url = search_types[query_type]
 
-	try:
-            print ("Search query attempted")
-            content_data = json.loads(urllib.request.urlopen(request).read())
-            is_no_results = False
-#TODO: make no_results work for specific search as well
-            if is_specific_search != True:
-                is_no_results = no_results(content_data)
-            if is_no_results:
-                print("No results found")
-                bot.sendMessage(update.message.chat_id, text = "No results found :(")
-            else:
-                # make proper call to function based on search type
-                if is_specific_search:
-                        format_and_send(bot, update, content_data, is_specific_search, search_type)
-                else:
-                        format_and_send(bot, update, content_data, is_specific_search)
-                print ("Search query successful")
-	except urllib.request.HTTPError as e:
-		print ("Search query failed")
-		print (e.code)
-		print (e.read())
+    print("Search URL created")
+    request = urllib.request.Request(search_url)
+    content_data = json.loads(urllib.request.urlopen(request).read())
+    #TODO: make check for no results
+
+    result = content_data[query_type + 's']['items'][0]['external_urls']['spotify']
+
+    return result
+
+# main function to handle all inline queries
+def inlinequery(bot, update):
+    query = update.inline_query.query
+    results = list()
+
+    # each new value will show up in the response to the user
+    results.append(InlineQueryResultArticle(id=uuid4(),
+        title='Track',
+        input_message_content=InputTextMessageContent(search(query, 'track'))))
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+        title='Artist',
+        input_message_content=InputTextMessageContent(search(query, 'artist'))))
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+        title='Album',
+        input_message_content=InputTextMessageContent(search(query, 'album'))))
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+        title='Playlist',
+        input_message_content=InputTextMessageContent(search(query, 'playlist'))))
+
+    update.inline_query.answer(results)
 
 def error(bot, update, error):
-	logger.warn('Update "%s" caused error "%s"' % (update, error))
+   logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 # main function needed to enable logging
 def main():
-	with open("telegram_token.txt", "r") as f:
-	    token = str(f.read()).rstrip()
+    with open("telegram_token.txt", "r") as f:
+       token = str(f.read()).rstrip()
 
-	updater = Updater(token)
+    updater = Updater(token)
 
-	dp = updater.dispatcher
+    dp = updater.dispatcher
 
-	dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("help", help))
 
-	dp.add_handler(CommandHandler("about", about))
+    dp.add_handler(CommandHandler("about", about))
 
-	dp.add_handler(MessageHandler(Filters.text, search))
+#       dp.add_handler(MessageHandler(Filters.text, search))
 
-	dp.add_error_handler(error)
+    dp.add_handler(InlineQueryHandler(inlinequery))
 
-	# begin long polling
-	updater.start_polling()
+    dp.add_error_handler(error)
 
-	updater.idle()
+    # begin long polling
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
-	print ("Bot is running...")
-	main()
+    print ("Bot is running...")
+    main()
